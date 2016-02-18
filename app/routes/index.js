3e1af6497c1c3f1a3088605c7a8ba8f691fb38d9
Dhirj.Gupta <zep.gupta@gmail.com>
2016-02-17 18:02:16 -0600

    basic server that will run aylien api summary service with private authentication keys

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var aylien = require('../lib/aylien')
+var urlencode = require('urlencode');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/summarizeURL/:url', function(req, res, next){
+	var url = urlencode.decode(req.params.url);
+	aylien.summarize({url: url}, function(err, response){
+		if(err){
+			console.log(err);
+			res.send(err);
+		}
+		else{
+			res.send(response);
+		}
+	})
+});
+
+router.get('/summarizeText/:title/:text', function(req, res, next){
+	var text = urlencode.decode(req.params.text);
+	aylien.summarize({title:req.params.title ,text: text}, function(err, response){
+		if(err){
+			console.log(err);
+			res.send(err);
+		}
+		else{
+			res.send(response);
+		}
+	})
 });
 
 module.exports = router;
